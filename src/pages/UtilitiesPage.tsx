e11d736318c7853ef200dcfecfb275b8b104f174
emanuelefavero <emafaux89@gmail.com>
2024-01-24 08:31:32 +0100

    add utilities - border

--- src/pages/UtilitiesPage.tsx
+++ src/pages/UtilitiesPage.tsx
@@ -13,6 +13,17 @@ export default function UtilitiesPage() {
 
       <h2>Background</h2>
       <div className='bg-success bg-gradient text-white p-2'>Background</div>
+
+      <h2 className='mt-3'>Border</h2>
+      <div className='border border-2 border-primary rounded-pill px-3 py-1 mb-2'>
+        Border
+      </div>
+      <div className='border border-2 border-primary rounded px-3 py-1 mb-2'>
+        Border
+      </div>
+      <div className='border border-2 border-primary rounded-start px-3 py-1 mb-2'>
+        Border
+      </div>
     </>
   )
 }
